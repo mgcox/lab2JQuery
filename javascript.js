f16bde83fd655e38128f4b5522adf61ca1aa6306
Jordan Wilbur <jjwilbur@gmail.com>
2016-09-27 01:33:02 +0000

    most functionality done

--- javascript.js
+++ javascript.js
@@ -1,31 +1,37 @@
-$(document).ready(function() {
-	var shows;
-	var myURL = "http://api.tvmaze.com/schedule?country=US&date=2016-09-23"
-	$.ajax({
-		url : myURL,
-		dataType : "json",
-		success : function(data){
-			console.log(data);
-			foreach(object in data){
-				if(object.show.network.name == "ABC"){
-					myURL = "http://www.omdbapi.com/?t=" + object.show.name + "&y=&plot=short&r=json";
-					$.ajax({
-						url : myURL,
-						dataType : "json",
-						success : function(data2){
-							console.log(data2);
-							var newShow{
-								"name": data2.title,
-								"plot": data2.plot,
-								"rating": data2.imdbRating,
-								"genre" : data2.Genre,
-								"image" : data2.Poster
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+  	
+  	$scope.shows = [];
+  	console.log($scope.shows);
+	$(document).ready(function() {
+		var myURL = "http://api.tvmaze.com/schedule?country=US&date=2016-09-23"
+		$.ajax({
+			url : myURL,
+			dataType : "json",
+			success : function(data){
+				console.log(data);
+				data.forEach(function(object){
+					if(object.show.network.name == "ABC"){
+						myURL = "http://www.omdbapi.com/?t=" + object.show.name + "&y=&plot=short&r=json";
+						$.ajax({
+							url : myURL,
+							dataType : "json",
+							success : function(data2){
+								console.log(data2);
+								var newShow = {
+									name: data2.Title,
+									plot: data2.Plot,
+									rating: data2.imdbRating,
+									genre: data2.Genre,
+									image: data2.Poster
+								}
+								$scope.shows.push(newShow);
+								$scope.$apply();
 							}
-							shows.add(newShow);
-						}
-					});
-				}
+						});
+					}
+				}, this);
 			}
-		}
+		});
 	});
 });
