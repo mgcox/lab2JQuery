2f3133947eb5ff89cfba672a055226034dcf69a3
Jordan Wilbur <jjwilbur@gmail.com>
2016-09-27 20:26:18 +0000

    added dropdown functionality

--- javascript.js
+++ javascript.js
@@ -1,37 +1,37 @@
 var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope) {
-  	
-  	$scope.shows = [];
-  	console.log($scope.shows);
-	$(document).ready(function() {
-		var myURL = "http://api.tvmaze.com/schedule?country=US&date=2016-09-23"
-		$.ajax({
-			url : myURL,
-			dataType : "json",
-			success : function(data){
-				console.log(data);
-				data.forEach(function(object){
-					if(object.show.network.name == "ABC"){
-						myURL = "http://www.omdbapi.com/?t=" + object.show.name + "&y=&plot=short&r=json";
-						$.ajax({
-							url : myURL,
-							dataType : "json",
-							success : function(data2){
-								console.log(data2);
-								var newShow = {
-									name: data2.Title,
-									plot: data2.Plot,
-									rating: data2.imdbRating,
-									genre: data2.Genre,
-									image: data2.Poster
-								}
-								$scope.shows.push(newShow);
-								$scope.$apply();
+app.controller("myCtrl", function($scope) {  	
+$scope.shows = [];
+$scope.GetListings = function(){
+	console.log("in GetListings");
+	var myURL = "http://api.tvmaze.com/schedule?country=US&date=2016-09-23";
+	$scope.shows = [];
+	$.ajax({
+		url : myURL,
+		dataType : "json",
+		success : function(data){
+			console.log(data);
+			data.forEach(function(object){
+				if(object.show.network.name == $scope.dropDownValue){
+					myURL = "http://www.omdbapi.com/?t=" + object.show.name + "&y=&plot=short&r=json";
+					$.ajax({
+						url : myURL,
+						dataType : "json",
+						success : function(data2){
+							console.log(data2);
+							var newShow = {
+								name: data2.Title,
+								plot: data2.Plot,
+								rating: data2.imdbRating,
+								genre: data2.Genre,
+								image: data2.Poster
 							}
-						});
-					}
-				}, this);
-			}
-		});
+							$scope.shows.push(newShow);
+							$scope.$apply();
+						}
+					});
+				}
+			}, this);
+		}
 	});
+};
 });
